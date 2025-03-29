bb4cd3098bd9add14d629fb52ba0fea4356d6407
5Salvador <153695334+5Salvador@users.noreply.github.com>
2025-03-29 19:07:20 +0530

    Order

--- src/pages/dashboard/users/OrderDetails.jsx
+++ src/pages/dashboard/users/OrderDetails.jsx
@@ -47,7 +47,7 @@ const OrderDetails = () => {
     
     return (
         <section className='section__container rounded p-6'>
-        <h2 className='text-2xl font-semibold mb-4'>Payment {order?.status}</h2>
+        <h2 className='text-2xl font-semibold mb-4'>Order {order?.status}</h2>
         <p className='mb-4'>Order Id: {order?.orderId}</p>
         <p className='mb-8'>Status: {order?.status}</p>
 
